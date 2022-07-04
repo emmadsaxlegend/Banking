004be6905c588b00a746962a20a71e466cb67f53
Emmanuel Okereke <eokereke47@gmail.com>
2022-07-04 13:34:46 +0100

    a

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(accountRoute)
 app.use(transactionRoute)
 
 //_____________serve react file _________________
-app.use(express.static(path.join(__dirname, 'frontend/build/index.html')));
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'frontend/build/index.html')));
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+// });
 
 const PORT = process.env.PORT || 5000
 
